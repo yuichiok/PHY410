230a5c4a54a2788aaa1919d7b9a40b73956fb53f
Yuichi <yuichiok@buffalo.edu>
2017-02-18 17:17:23 -0500

    released Problem2

---
+++ Homework2/Problem2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <limits>
+
+int main(void){
+    
+    int i1 = 2, i2 = 5, i3 = -3;
+    double d1 = 2.0, d2 = 5.0, d3 = -0.5;
+    
+    std::cout << "(a)i1 + (i2 * i3) = " << i1 + (i2 * i3) << "\n";
+    std::cout << "(b)i1 * (i2 + i3) = " << i1 * (i2 + i3) << "\n";
+    std::cout << "(c)i1 / (i2 + i3) = " << i1 / (i2 + i3) << "\n";
+    std::cout << "(d) i1 / i2 + i3 = "  << i1 / i2 + i3 << "\n";
+    std::cout << "(e) 3 + 4 + 5 / 3 = " << 3 + 4 + 5 / 3 << "\n";
+    std::cout << "(f) (3 + 4 + 5) / 3 = " << (3 + 4 + 5) / 3 << "\n";
+    std::cout << "(g) d1 + (d2 * d3) = " << d1 + (d2 * d3) << "\n";
+    std::cout << "(h) d1 + d2 * d3 = " << d1 + d2 * d3 << "\n";
+    std::cout << "(i) d1 / d2 - d3 = " << d1 / d2 - d3 << "\n";
+    std::cout << "(j) d1 / (d2 - d3) = " << d1 / (d2 - d3) << "\n";
+    std::cout << "(k) d1 + d2 + d3 / 3 = " << d1 + d2 + d3 / 3 << "\n";
+    std::cout << "(l) (d1 + d2 + d3) / 3 = " << (d1 + d2 + d3) / 3 << "\n";
+    std::cout << "(m) d1 + d2 + (d3 / 3) = " << d1 + d2 + (d3 / 3) << "\n";
+    std::cout << "(n) 3 * (d1 + d2) * (d1 - d3) = " << 3 * (d1 + d2) * (d1 - d3) << "\n";
+    
+    
+    return 0;
+}
