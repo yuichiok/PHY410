d3448ea121898b4abcd2cb4d0a6b059356bb1f6f
Yuichi <yuichiok@buffalo.edu>
2017-02-26 00:41:35 -0500

    Problem1: prints degenerate vectors

--- Homework3/Problem1.cpp
+++ Homework3/Problem1.cpp
@@ -6,49 +6,52 @@ float magnitude(float x, float y);
 
 int main(void){
     
-    //enter n
-    //unsigned int n=0;
-    
-    /*
-    while(1){
-        std::cout << "Enter an interger between 0 to 20: ";
-        std::cin >> n;
-        if( !std::cin.fail() ) break; //breaks when undesired input has been entered
-        std::cout << "Error: Number must be between 0 to 20 \n";
-        std::cin.clear();    //reset cin value
-        std::cin.ignore( 1024, '\n' ); //disregard strings
-    }*/
-    
+    //declaration of variables and vectors
+    std::vector<float> magvec, vecx, vecy;
     float xi = 0., yi = 0.;
     float magmin = std::numeric_limits<float>::max(), xmin = 0., ymin = 0.;
     float mag = 0;
+    float magval = 0;
     int i = 0;
+    int n_magvec =0;
     
     std::cout << "Enter vectors: ";
     
+    //input
     while( std::cin >> xi >> yi ) {
         
-        if( std::cin.fail() ) break;
+        //if( std::cin.fail() ) break;
         std::cout << "{" << xi << ", " << yi << "}" << std::endl;
-        mag = magnitude(xi,yi);
-        if(mag < magmin){
-            magmin = mag;
-            xmin = xi;
-            ymin = yi;
-        }else if(mag == magmin){
-            
-            
-        }
+        vecx.push_back(xi); //push inputs to vector vecx
+        vecy.push_back(yi); //push inputs to vector vecy
+        mag = magnitude(xi,yi); //calculation of magnitude
+        magvec.push_back(mag);
+        
+        if( mag < magmin ) magmin = mag;
+        
         std::cout << "magnitude = " << mag << std::endl;
         
     }
-    
+
     std::cout << std::string(60, '*') << std::endl;
     
-    std::cout << "vector with min magnitude: {" << xmin << ", " << ymin << "}" << std::endl;
+    std::cout << "vectors:";
+    
+    n_magvec = magvec.size();
     
     
+    for(int k=0; k<n_magvec; k++){
+        
+        magval = magvec[k];
+        
+        if(magmin == magval){
+            std::cout << " {" << vecx[k] << ", " << vecy[k] << "}";
+        }
+    }
+    
+    std::cout << "\n" << "magnitude = " << magmin << std::endl;
     std::cout << "DONE!" << std::endl;
+    
     return 0;
 }
 
