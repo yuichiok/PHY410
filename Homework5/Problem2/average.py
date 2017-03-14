2fb3352ff730ee58726bc7401c99f66b13f04a05
Yuichi <yuichiok@buffalo.edu>
2017-03-14 01:25:26 -0400

    late night submission

--- Homework5/Problem2/average.py
+++ Homework5/Problem2/average.py
@@ -2,8 +2,19 @@ import sys
 
 file_name = sys.argv[1]
 
-with open(file_name) as f:
-    for l in f.readlines():
-        print l.strip().split(",")
-        break
-        f.close()
+def main(name):
+    
+    record = open(name)
+    sum = 0.0
+    n = 0
+
+    for line in record:
+        print line
+        n += 1
+        l = line.split(',')
+        score = float(l[2])
+        sum += score
+    
+    return sum / n
+
+print("average = " + str(main(file_name)))
